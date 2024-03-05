773a46cb10717f270ade741c21f92baad761ee06
ftgr <fernandestgr@gmail.com>
2024-03-05 06:12:06 -0400

    Fineshed workbook 2.2

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -5,18 +5,26 @@ public class Biography {
 
 
         // name (text)
+        String name = "Sam";
         // age (whole number)
+        int age = 35;
         // country (text)
+        String country = "England";
         // sport (text)
+        String sport = "Basketball";
         // hours (whole number)
+        int hours = 2;
         // game (text)
+        String game = "Checkers";
         // subject (text)
+        String subject = "Chemistry";
         // grade (character)
+        char grade = 'B';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from "+country+". "); // Complete this line. It should print: My name is <name>. I'm <age> years old, and I'm from <country> ");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day"); // Complete this line. It should print: My favourite sport is <sport>. I play for <hours> hours a day");
+        System.out.println("When I'm tired, I like to play " + game + "."); // Complete this line. It should print: When I'm tired, I like to play <game.");
+        System.out.println("In school, my favourite subject was "+ subject + ", I scored a " + grade + "."); // Complete this line. It should print: In school, my favourite subject was <subject>, I scored a <grade>.");
 
         // Compare your result to what's on Learn the Part.
     }
