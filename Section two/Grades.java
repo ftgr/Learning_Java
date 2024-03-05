9e842b9046ef9bbed2d0b16bfe43bfa944348a8e
ftgr <fernandestgr@gmail.com>
2024-03-05 06:02:52 -0400

    Char

---
+++ Section two/Grades.java
@@ -0,0 +1,9 @@
+public class Grades {
+   public static void main(String[] args) {
+    char potions = 'A';
+    char arithmancy = 'B';
+    char charms = 'C';
+
+    System.out.println("Potions: " + potions + " Arithmancy: " + arithmancy + " Charms: " + charms);
+   } 
+}
