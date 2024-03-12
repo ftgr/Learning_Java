d1cdd7e0415903f6403800ebc45f74d1dcb3610b
Fernanes Rodrigues <fernandestgr@gmail.com>
2024-03-12 13:39:54 -0400

    Long type

---
+++ Section two/WholeNumbers.java
@@ -0,0 +1,8 @@
+public class WholeNumbers{
+    public static void main(String[] args) {
+        long globalPopulation = 8_000_000_000_000L;
+        System.out.println("The global population is " + globalPopulation);
+        long dailyGoogleSearches = 8_500_000_000_000L;
+        System.out.println("Daily Google Searches: " + dailyGoogleSearches + "google searches");
+    }
+}
