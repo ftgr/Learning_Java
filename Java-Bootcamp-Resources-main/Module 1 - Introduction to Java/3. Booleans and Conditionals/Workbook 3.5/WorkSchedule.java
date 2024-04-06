8d361038df03803fab263ce0ea0ceaa0f7f2de79
Fernanes Rodrigues <fernandestgr@gmail.com>
2024-04-06 10:51:03 -0400

    Finished workbook 3;5

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -3,10 +3,17 @@ public class WorkSchedule {
         
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
-        int day = 3;   //3rd day of the week...
-        boolean holiday = true;
+        int day = 6;   //3rd day of the week...
+        boolean holiday = false;
          
         // IF - ELSE IF - ELSE HERE!
+        if (holiday) {
+            System.out.println("woohoo, no work!");
+        } else if (day == 6 || day == 7) {
+            System.out.println("it's the weekend, no work!");
+        } else {
+            System.out.println("Wake up at 7:00 :(");
+        }
         
         
         }
