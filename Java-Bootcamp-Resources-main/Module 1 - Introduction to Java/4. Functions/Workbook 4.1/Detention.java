74ff793c1081ecdadad6c6c910d2eda41cad8ed9
Fernanes Rodrigues <fernandestgr@gmail.com>
2024-04-08 07:49:31 -0400

    Finished workbook 4.1

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
@@ -1,13 +1,13 @@
 public class Detention {
     public static void main(String[] args) {
 
-        //what Bart did so far
-        System.out.println("I will not copy and paste code.");
-        System.out.println("I will use Camel Case when writing class names.");
-        System.out.println("I will use lower Camel Case when writing function names.");
-        System.out.println("I will use lower Camel Case when writing variables names.\n");
-
-        // Task 2 – call the function 6 times. 
+               // Task 2 – call the function 6 times. 
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
    }
 
 
@@ -20,6 +20,13 @@ public class Detention {
       * Inside the function:
       *   1. prints the four lines       
       */
+   public static void printLines() {
+      //what Bart did so far
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function names.");
+        System.out.println("I will use lower Camel Case when writing variables names.\n");
+   }
 
 
 }
