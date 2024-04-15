f197c928db376bd298bfcbc4ae2fdf179b0e32a9
Fernanes Rodrigues <fernandestgr@gmail.com>
2024-04-15 09:33:42 -0400

    Workbook 5.2

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
-        // See Learn the Part for the instructions.
+
+        //I will not forget semi-colons.
         
+        String line = scanner.nextLine();
+
+        for (int i = 1; i <= 99; i++) {
+            System.out.println(i + ". " + line);
+        }
+        
+        scanner.close();
     }
 }
