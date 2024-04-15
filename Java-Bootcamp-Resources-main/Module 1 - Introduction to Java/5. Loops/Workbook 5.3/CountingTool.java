4ef30f1257e53b6dc35746c7d1a47aacc4ff1e12
Fernanes Rodrigues <fernandestgr@gmail.com>
2024-04-15 09:43:00 -0400

    Workbook 5.3

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -4,7 +4,12 @@ public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // See Learn the Part for the instructions.
+        System.out.print("Hi Timmy! What number would you like to count up to?: ");
+        int number = scan.nextInt();
+        for (int i = 0; i <= number; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
 
         scan.close();
     }
