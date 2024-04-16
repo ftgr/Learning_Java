34b5f5af3fe91d10987d4020ca0c393f03b52b41
Fernanes Rodrigues <fernandestgr@gmail.com>
2024-04-16 11:04:14 -0400

    Blackjack

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
@@ -10,16 +10,28 @@ public class Blackjack {
         System.out.println("We shall see..");
         System.out.println("..Ready? Press anything to begin!");
         //Task 3 – Wait for the user to press enter.
+        scan.nextLine();
         //Task 4 – Get two random cards.
         //       – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
+        int yourCard1 = drawRandomCard();
+        int yourCard2 = drawRandomCard();
+        System.out.println("\n You get a \n" + cardString(yourCard1) + "and a \n" + cardString(yourCard2));
 
         //Task 5 – Print the sum of your hand value.
         //       – print: your total is: <hand value>
-
+        int total = Math.min(yourCard1, 10) + Math.min(yourCard2, 10);
+        System.out.println("your total is: " + total);
         
         //Task 6 – Get two random cards for the dealer.
         //       – Print: The dealer shows \n" + <first card> + "\nand has a card facing down \n" + <facedown card>
         //       – Print: \nThe dealer's total is hidden
+        int dealerCard1 = drawRandomCard();
+        int dealerCard2 = drawRandomCard();        
+
+        System.out.println("\n The dealer shows \n" + cardString(dealerCard1) + "\nand has a card facing down \n"
+        + faceDown());
+        int dealerTotal = Math.min(dealerCard1, 10) + Math.min(dealerCard2, 10);
+        System.out.println("\nThe dealer's total is hidden");
 
        
         //Task 8 – Keep asking the player to hit or stay (while loop).
@@ -30,9 +42,47 @@ public class Blackjack {
         //             - print: your new total is <total>
 
         //       2. Once the player stays, break the loop. 
+        while (true) {
+            String option = hitOrStay();
 
+            if (option.equals("stay")) {
+                break;
+            }
+            int newCard = drawRandomCard();
+            total += Math.min(newCard, 10);
+            System.out.println("\nYou get a\n" + cardString(newCard));
+            System.out.println("your total is " + total);
+
+            if (total > 21) {
+                System.out.println("Bust! Player loses");
+                System.exit(0);
+            }
+        }
         
         //For tasks 9 to 13, see the article: Blackjack Part II. 
+        System.out.println("\nDealer's turn");
+        System.out.println("\n The dealer's cards are \n" + cardString(dealerCard1) 
+        + "\n and a \n" + cardString(dealerCard2));
+        System.out.println("Dealer's total is " + dealerTotal);
+
+        while (dealerTotal < 17) {
+            int newCard = drawRandomCard();
+
+            dealerTotal += Math.min(newCard, 10);
+            System.out.println("\n Dealer gets a \n" + cardString(newCard));
+            System.out.println("Dealer's total is " + dealerTotal);
+        }
+        
+        if (dealerTotal > 21) {
+            System.out.println("Bust! Dealer loses.");
+            System.exit(0);
+        }
+
+        if (total > dealerTotal) {
+            System.out.println("Player wins!");
+        } else {
+            System.out.println("Dealer wins!");
+        }
          scan.close();
 
     }
@@ -45,6 +95,11 @@ public class Blackjack {
      *   1. Gets a random number between 1 and 13.
      *   2. Returns a card.
      */
+    public static int drawRandomCard(){
+        double randomNumber = Math.random()*13;
+        int randomInt = (int) randomNumber + 1;
+        return randomInt;
+    }
 
     /** Task 2 – make a function that returns a String drawing of the card.
      * Function name – cardString
@@ -54,6 +109,114 @@ public class Blackjack {
      * Inside the function:
      *   1. Returns a String drawing of the card.
      */
+    public static String cardString(int cardNumber){
+        switch (cardNumber) {
+            case 1:
+                return "   _____\n" +
+                        "  |A _  |\n" +
+                        "  | ( ) |\n" +
+                        "  |(_'_)|\n" +
+                        "  |  |  |\n" +
+                        "  |____V|\n";
+
+            case 2:
+                return "   _____\n" +
+                        "  |2    |\n" +
+                        "  |  o  |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____Z|\n";
+            case 3:
+                return "   _____\n" +
+                        "  |3    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____E|\n";
+
+            case 4:
+                return "   _____\n" +
+                        "  |4    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  | o o |\n" +
+                        "  |____h|\n";
+
+            case 5:
+                return "   _____ \n" +
+                        "  |5    |\n" +
+                        "  | o o |\n" +
+                        "  |  o  |\n" +
+                        "  | o o |\n" +
+                        "  |____S|\n";
+
+            case 6:
+                return "   _____ \n" +
+                        "  |6    |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  |____6|\n";
+
+            case 7:
+                return "   _____ \n" +
+                        "  |7    |\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |____7|\n";
+
+            case 8:
+                return "   _____ \n" +
+                        "  |8    |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  |____8|\n";
+
+            case 9:
+                return "   _____ \n" +
+                        "  |9    |\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |____9|\n";
+
+            case 10:
+                return "   _____ \n" +
+                        "  |10  o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |___10|\n";
+
+            case 11:
+                return "   _____\n" +
+                        "  |J  ww|\n" +
+                        "  | o {)|\n" +
+                        "  |o o% |\n" +
+                        "  | | % |\n" +
+                        "  |__%%[|\n";
+
+            case 12:
+                return "   _____\n" +
+                        "  |Q  ww|\n" +
+                        "  | o {(|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%O|\n";
+
+            case 13:
+                return "   _____\n" +
+                        "  |K  WW|\n" +
+                        "  | o {)|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%>|\n";
+            default:
+                return "Invalid option";
+        }
+    }
 
     public static String faceDown() {
         return
@@ -75,5 +238,14 @@ public class Blackjack {
      *      Please write 'hit' or 'stay'
      *   3. Returns the user's option 
      */
+    public static String hitOrStay(){
+        System.out.println("Do you want to hit or stay?");
+        String choice = scan.nextLine();
+        while (!choice.equals("hit") || !choice.equals("stay")){
+            System.out.println("Please write 'hit' or 'stay'");
+            choice = scan.nextLine();
+        }
+        return choice;
+    }
     }
 
